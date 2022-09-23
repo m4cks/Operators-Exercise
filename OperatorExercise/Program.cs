5d888df3204319bc40c921a6f9f86cd6ea9af909
Max Schlapfer <max.schlapfer@gmail.com>
2022-09-22 23:42:28 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //DIVISION
+            DivisionExercise();
+
+            //AREA OF CIRCLE
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a cicle with radius of {radius} is {area}");
+
+            //THOUGHT EXERCISE
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+            //++i will return 3+1=4
+            //j++ will return 4+1=5
+            //therefore k will return 4*5 = 
+            Console.WriteLine(k);
+        }
+
+        //SECTION: DIVISION
+        public static void DivisionExercise() 
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+        }
+
+        //SECTION: AREA OF CIRCLE
+        public static double AreaOfCircle(double radius)
+        {   
+            return Math.PI * Math.Pow(radius, 2);
         }
     }
 }
